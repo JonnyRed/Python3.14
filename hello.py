5354c79dc2d2e3c7f6c1afa5b01068ade2ae91bd
John Reynolds <jjmreynolds@gmail.com>
2026-01-05 23:25:10 +0100

    Initial commit

---
+++ hello.py
@@ -0,0 +1,17 @@
+import sys
+import sympy
+import seaborn as sns
+import matplotlib
+import pandas as pd
+import numpy as np
+import scipy
+
+print("Hello, World!")
+
+print(f"Python version: {sys.version.split()[0]}")
+print(f"SymPy:      {sympy.__version__}")
+print(f"Seaborn:    {sns.__version__}")
+print(f"Matplotlib: {matplotlib.__version__}")
+print(f"Pandas:     {pd.__version__}")
+print(f"NumPy:      {np.__version__}")
+print(f"SciPy:      {scipy.__version__}")
